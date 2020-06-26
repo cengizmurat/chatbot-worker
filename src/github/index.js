ec16ce58152eedd4bb8b57aa638821cf3783dfc9
Murat Cengiz <mcengiz240296@gmail.com>
2020-06-26 18:33:42 +0200

    Import repository SGitHub

--- src/github/index.js
+++ src/github/index.js
@@ -1,10 +1,13 @@
+const fs = require('fs')
 const express = require('express')
 const axios = require('axios')
+const git = require('nodegit')
 
 const logger = require('../logger')
 const config = require('../../config.js')
 const router = express.Router()
 
+router.post('/repos/:orgId/:repoName/import', importRepository)
 router.post('/*', postAll)
 router.get('/*', getAll)
 router.put('/*', putAll)
@@ -15,6 +18,45 @@ const axiosConfig = {
     headers: { Authorization: `Bearer ${config.GITHUB_TOKEN}` },
 }
 const axiosInstance = axios.create(axiosConfig)
+
+const repoDirectory = './imports'
+
+async function importRepository(req, res, next) {
+    try {
+        const orgId = req.params['orgId']
+        const repoName = req.params['repoName']
+        const {destination_url, vcs_url} = req.body
+
+        const repoPath = `${repoDirectory}/${orgId}/${repoName}`
+        try {
+            if (fs.lstatSync(repoPath).isDirectory()) {
+                fs.rmdirSync(repoPath, {recursive: true})
+            }
+        } catch (e) {
+        }
+
+        const repo = await git.Clone(vcs_url, repoPath)
+        await git.Remote.setPushurl(repo, 'origin', destination_url)
+
+        const remote = await repo.getRemote('origin')
+        const refs = await repo.getReferences()
+        const refSpecs = refs.map(ref => `${ref.toString()}:${ref.toString()}`)
+
+        await remote.push(refSpecs, {
+            callbacks: {
+                credentials: gitCredentials,
+            }
+        })
+
+        await res.json({result: 'OK'})
+    } catch (e) {
+        next(e)
+    }
+}
+
+function gitCredentials(url, user) {
+    return git.Cred.userpassPlaintextNew(config.GITHUB_TOKEN, 'x-oauth-basic')
+}
 
 async function getAll(req, res, next) {
     try {
