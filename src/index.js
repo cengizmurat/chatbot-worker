f68f42266a1d7881b6eaff75ae701ad9ed31563a
Murat Cengiz <mcengiz240296@gmail.com>
2020-09-03 11:03:28 +0200

    Debug

--- src/index.js
+++ src/index.js
@@ -101,6 +101,7 @@ function logResponseBody(req, res) {
 }
 
 async function handleError(err, req, res, next) {
+    console.error(err)
     logger.log(err, 'ERROR')
     const response = err.response
     if (response && response.data) {
